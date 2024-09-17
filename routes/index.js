56ab6b6ec2608760f163026e9da06bb47d539374
Paul Baumert <baumert.trey@gmail.com>
2024-09-17 13:17:52 -0500

    moved index.js into routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./api/userRoutes');
+const thoughtRoutes = require('./api/thoughtRoutes');
 
 router.use('/api/users', userRoutes);
 router.use('/api/thoughts', thoughtRoutes);
